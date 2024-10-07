76e51b7f5f64d7d2b2eaf01c5ca75879a8a8774d
Mike Wyatt <wyatt.mike@gmail.com>
2024-10-07 19:42:18 -0300

    add --version support

--- src/main.go
+++ src/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	_ "embed"
+	"encoding/json"
 	"log"
 	"os"
 
@@ -9,10 +11,27 @@ import (
 	"nvrh/src/client"
 )
 
+//go:embed manifest.json
+var manifestData []byte
+
+type Manifest struct {
+	Name             string `json:"name"`
+	Version          string `json:"version"`
+	ShortDescription string `json:"shortDescription"`
+}
+
 func main() {
+	var manifest Manifest
+	if err := json.Unmarshal(manifestData, &manifest); err != nil {
+		log.Fatalf("Error reading manifest: %v", err)
+	}
+
 	app := &cli.App{
-		Name:  "nvrh",
-		Usage: "Helps work with a remote nvim instance",
+		Name: manifest.Name,
+		// These fields are named kind of strange. The `Usage` field is paired with
+		// the `Name` when running `--help`.
+		Usage:   manifest.ShortDescription,
+		Version: manifest.Version,
 
 		Commands: []*cli.Command{
 			&client.CliClientCommand,
