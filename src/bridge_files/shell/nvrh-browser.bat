9a1175eee4a7ab61d7c5d0eb7ffc3997adcb27d5
Mike Wyatt <wyatt.mike@gmail.com>
2026-01-28 00:49:57 -0400

    WIP script safety

--- src/bridge_files/shell/nvrh-browser.bat
+++ src/bridge_files/shell/nvrh-browser.bat
@@ -1,4 +1,15 @@
 @echo off
-set SOCKET_PATH=%s
+setlocal enabledelayedexpansion
 
-start "" nvim --server "%%SOCKET_PATH%%" --remote-send "<cmd>lua _G._nvrh.open_url([[%%1]])<cr>"
+set "URL=%%~1"
+
+:: Escape backslashes
+set "URL=!URL:\=\\!"
+
+:: Escape double quotes
+set "URL=!URL:"=\"!"
+
+set "SOCKET_PATH=%s"
+
+REM start "" nvim --server "%%SOCKET_PATH%%" --remote-expr "v:lua._G._nvrh.open_url(\"!URL!\")" > nul 2>&1
+nvim --server "%%SOCKET_PATH%%" --remote-expr "v:lua._G._nvrh.open_url(\"!URL!\")" > nul 2>&1
--- src/bridge_files/shell/nvrh-editor.bat
+++ src/bridge_files/shell/nvrh-editor.bat
@@ -1,14 +1,24 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set "SOCKET_PATH=%s"
-set "LOCK_FILE=%%TEMP%%\nvrh-editor-%%RANDOM%%.lock"
-
 set "FILE_PATH=%%~1"
 set "LINE=%%~2"
 set "COL=%%~3"
 
-nvim --server "%%SOCKET_PATH%%" --remote-send "<cmd>lua _G._nvrh.edit_with_lock([[%%FILE_PATH%%]], [[%%LOCK_FILE%%]], [[%%LINE%%]], [[%%COL%%]])<cr>"
+:: Escape backslashes
+set "FILE_PATH=!FILE_PATH:\=\\!"
+set "LINE_NUMBER=!LINE_NUMBER:\=\\!"
+set "COLUMN_NUMBER=!COLUMN_NUMBER:\=\\!"
+
+:: Escape double quotes
+set "FILE_PATH=!FILE_PATH:"=\"!"
+set "LINE_NUMBER=!LINE_NUMBER:"=\"!"
+set "COLUMN_NUMBER=!COLUMN_NUMBER:"=\"!"
+
+set "SOCKET_PATH=%s"
+set "LOCK_FILE=%%TEMP%%\nvrh-editor-%%RANDOM%%.lock"
+
+nvim --server "%%SOCKET_PATH%%" --remote-expr "v:lua._G._nvrh.edit_with_lock(\"!FILE_PATH!\", \"!LOCK_FILE!\", \"!LINE!\", \"!COL!\")" > nul 2>&1
 
 pathping 127.0.0.1 -n -q 1 -p 100 >nul
 
