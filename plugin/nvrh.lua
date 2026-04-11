96cdb0bc7c99e1fb5acb0f9281accce38433023c
Mike Wyatt <wyatt.mike@gmail.com>
2026-04-11 19:54:21 -0300

    why not both

--- plugin/nvrh.lua
+++ plugin/nvrh.lua
@@ -1,11 +1,23 @@
-vim.api.nvim_create_user_command('NvrhOpen', function(args)
+--- @param args string[]
+local function nvrh_connect(args)
   local cmd = { 'nvrh', 'client', 'from-neovim', vim.v.servername }
 
-  for _, arg in ipairs(args.fargs) do
+  for _, arg in ipairs(args) do
     table.insert(cmd, arg)
   end
 
   vim.system(cmd, { detach = true })
+end
+
+vim.api.nvim_create_user_command('NvrhOpen', function(args)
+  nvrh_connect(args.fargs)
+end, {
+  nargs = '*',
+  force = true,
+})
+
+vim.api.nvim_create_user_command('NvrhConnect', function(args)
+  nvrh_connect(args.fargs)
 end, {
   nargs = '*',
   force = true,
