0142ff48b2f5d5e9149e41572d1535d7a0b14a62
Mike Wyatt <wyatt.mike@gmail.com>
2026-04-11 20:08:59 -0300

    nah

--- plugin/nvrh.lua
+++ plugin/nvrh.lua
@@ -9,13 +9,6 @@ local function nvrh_connect(args)
   vim.system(cmd, { detach = true })
 end
 
-vim.api.nvim_create_user_command('NvrhOpen', function(args)
-  nvrh_connect(args.fargs)
-end, {
-  nargs = '*',
-  force = true,
-})
-
 vim.api.nvim_create_user_command('NvrhConnect', function(args)
   nvrh_connect(args.fargs)
 end, {
