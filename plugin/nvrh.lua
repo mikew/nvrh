5a2c58c1c27e8d8831260674eb0539750ad75a77
Mike Wyatt <wyatt.mike@gmail.com>
2026-04-20 21:34:23 -0300

    can't run NvrhConnect in nvrh session

--- plugin/nvrh.lua
+++ plugin/nvrh.lua
@@ -1,5 +1,20 @@
 --- @param args string[]
 local function nvrh_connect(args)
+  if _G._nvrh then
+    local hint_cmd = { 'nvrh', 'client', 'open' }
+    for _, arg in ipairs(args) do
+      table.insert(hint_cmd, arg)
+    end
+
+    vim.notify(
+      'Already connected to nvrh, please manually run `'
+        .. table.concat(hint_cmd, ' ')
+        .. '` in your terminal to start a new session.',
+      vim.log.levels.WARN
+    )
+    return
+  end
+
   local cmd = { 'nvrh', 'client', 'from-neovim', vim.v.servername }
 
   for _, arg in ipairs(args) do
